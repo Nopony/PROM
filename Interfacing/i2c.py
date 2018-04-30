f8eae49b382a4b448fd6afd60d4ba3f1d2950392
Brian Camacho <hello@briancamacho.me>
2018-04-30 12:33:15 +0100

    Testing adc read module

--- Interfacing/i2c.py
+++ Interfacing/i2c.py
@@ -2,20 +2,24 @@ import smbus #I2C library
 import time
 import threading
 
-I2C_ADDR = 0x20 #I2C base addres
-sbus = smbus.SMBus(1) #enable I2C bus
+I2C_B_ADDR = 0x20 #I2C base address
+I2C_A_ADDR = 0x21 #built-in I2C with ADC address
+bus = smbus.SMBus(1) #enable I2C bus
 
 BTN_MASK = 0b00001000
 BTN_POLLING_DELAY = 0.05
-btn_state = False
 
+ADC_COMMAND = 0b00010000
+
+btn_state = False
+mic_state = False
 
 def setLeds(yellow=False, green=False, red=False):
 	ledVals = 0x00
 	for idx, colour in enumerate((yellow, green, red)):
 		if(colour):
 			ledVals = ledVals | (0b1 << idx)
-	bus.write_byte(I2C_ADDR, ledVals)
+	bus.write_byte(I2C_B_ADDR, ledVals)
 
 def getButton():
 	global btn_state
@@ -28,7 +32,7 @@ def getButton():
 def checkButton():
 	global btn_state
 
-	byte = sbus.read_byte(I2C_ADDR)
+	byte = bus.read_byte(I2C_B_ADDR)
 	masked = BTN_MASK & byte
 
 	if(masked == 0):
@@ -36,14 +40,24 @@ def checkButton():
 
 	threading.Timer(BTN_POLLING_DELAY, checkButton).start()
 
-checkButton()
+#checkButton()
+
+
+def checkMic():
+	global mic_state
+
+	bus.write_byte(I2C_A_ADDR, ADC_COMMAND)
+	result = bus.read_byte(I2C_A_ADDR)
+
+	print(result)
+
+
 
 while True:
-	print(getButton())
+	checkMic()
+	time.sleep(0.02)
 
-	time.sleep(1)
-
-#bus.write_byte( I2C_ADDR, LED_ON ) #set port to 0
+#bus.write_byte( I2C_B_ADDR, LED_ON ) #set port to 0
 #time.sleep(1) #wait 1 sec
-#bus.write_byte( I2C_ADDR, LED_OFF ) #set port to 1
+#bus.write_byte( I2C_B_ADDR, LED_OFF ) #set port to 1
 #time.sleep(1) 
