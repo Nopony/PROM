68022d8ca1d5267ad865fe69fbd4a73a499fa382
perlinwarp@gmail.com <perlinwarp@gmail.com>
2018-04-30 11:35:16 +0100

    Added convolutions

--- processing.py
+++ processing.py
@@ -3,6 +3,47 @@ from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 
+def maxGreen(im):
+	recordGreen = 0
+
+	(width, height) = im.shape[0:2]
+	print(width,height)
+	for x in range(0,width):
+	    for y in range(0,height):
+	        pixel = im[x-1][y-1]
+	        red = pixel[0]
+	        green = pixel[1]
+	        blue = pixel[2]
+	        if(green > recordGreen):
+	        	print('boi')
+	        	recordGreen = green
+	        	print('Vals =', im[x-1][y-1])
+	        	im[x-1][y-1] = [255,0,0]
+	imgplot = plt.imshow(im)
+	plt.show()
+	return recordGreen
+
+def detect(im):
+
+	(height,width) = im.shape[0:2]
+	print(height,width)
+	for x in range(0,width):
+	    for y in range(0,height):
+	        pixel = im[y][x]
+	        red = pixel[0]
+	        green = pixel[1]
+	        blue = pixel[2]
+	        if(green > red and green > blue and green > 200):
+	        	print('Pos = ', x, y, 'Vals =', im[y][x])
+	        	im[y:][x:] = [244,63,232]
+	#im[0:255][0:255] = [0,0,0]
+	imgplot = plt.imshow(im)
+	plt.show()
+
+	return im
+
+
+
 #Opening an image without libs
 # name = "name"
 # os.system("fswebcam "+name+".jpg")
@@ -10,14 +51,9 @@ import matplotlib.pyplot as plt
 # os.system("feh " + name + ".ppm")
 
 #Opening the image
-im = Image.open("testimage.jpg")
-img2arr = np.array(im)
+image = Image.open("testimage.jpg")
+im = np.array(image)
 
-(width, height) = img2arr.shape[0:2]
-print(width,height)
-for x in range(width):
-    for y in range(height):
-        
+print(detect(im))
 
-imgplot = plt.imshow(im)
-plt.show()
+
