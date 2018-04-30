8e3b94ae90549852f78e747f3d525a5446a15be4
B Camacho <bc1018@pc787s.cs.york.ac.uk>
2018-04-30 14:58:52 +0100

    Added reading ADC values

--- Interfacing/i2c.py
+++ Interfacing/i2c.py
@@ -10,6 +10,13 @@ BTN_MASK = 0b00001000
 BTN_POLLING_DELAY = 0.05
 
 ADC_COMMAND = 0b00010000
+ADC_READ_MASK = 0b0000111111111111
+ADC_UPPER_BYTE_MASK = 0b1111111100000000
+ADC_MAX_VALUE = 3852
+ADC_THRESHOLD_VOLTAGE = 2.5
+ADC_MAX_VOLTAGE = 3
+ADC_THRESHOLD_VALUE = (ADC_THRESHOLD_VOLTAGE / ADC_MAX_VOLTAGE) * ADC_MAX_VALUE
+ADC_POLLING_DELAY = 0.05
 
 btn_state = False
 mic_state = False
@@ -20,7 +27,7 @@ def setLeds(yellow=False, green=False, red=False):
 		if(colour):
 			ledVals = ledVals | (0b1 << idx)
 	bus.write_byte(I2C_B_ADDR, ledVals)
-
+# returns True if button was pressed since the last time getButton was called
 def getButton():
 	global btn_state
 	if(btn_state):
@@ -39,25 +46,35 @@ def checkButton():
 		btn_state = True
 
 	threading.Timer(BTN_POLLING_DELAY, checkButton).start()
-
-#checkButton()
+checkButton()
 
 
 def checkMic():
 	global mic_state
 
 	bus.write_byte(I2C_A_ADDR, ADC_COMMAND)
-	result = bus.read_byte(I2C_A_ADDR)
+	result = bus.read_word_data(I2C_A_ADDR, 0x00)
+	
+	result &= ADC_READ_MASK
+	result = ( (result & ADC_UPPER_BYTE_MASK) >> 8 ) | ( (result & ~ADC_UPPER_BYTE_MASK ) << 8 )
 
-	print(result)
+	if(result > ADC_THRESHOLD_VALUE):
+		mic_state = True
 
+	threading.Timer(ADC_POLLING_DELAY, checkMic).start()
+checkMic()
+
+# returns True if mic voltage exceeded threshold since last time getMic was called
+def getMic():
+	global mic_state
+	
+	if(mic_state):
+		mic_state = False
+		return True
+	return mic_state
 
 
 while True:
-	checkMic()
-	time.sleep(0.02)
+	print(getMic())
 
-#bus.write_byte( I2C_B_ADDR, LED_ON ) #set port to 0
-#time.sleep(1) #wait 1 sec
-#bus.write_byte( I2C_B_ADDR, LED_OFF ) #set port to 1
-#time.sleep(1) 
+	time.sleep(3)
