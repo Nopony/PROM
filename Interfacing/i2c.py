9489fc70ea139f1a847fcf04febffc3315cb4817
Brian Camacho <hello@briancamacho.me>
2018-05-03 17:04:39 +0100

    Add PID for testing

--- Interfacing/i2c.py
+++ Interfacing/i2c.py
@@ -17,20 +17,31 @@ c = ConfigParser.ConfigParser()
 c.read('./constants.ini')
 bus = smbus.SMBus(1) #enable I2C bus
 
+I2C_ADDR_A = int(c.get('I2C', 'ADDR_A'), 16)
+I2C_ADDR_B = int(c.get('I2C', 'ADDR_B'), 16)
 
+ADC_COMMAND_CH1 = 0b00010000
+ADC_COMMAND_CH2 = 0b00100000
+ADC_READ_MASK = 0b0000111111111111
+ADC_UPPER_BYTE_MASK = 0b1111111100000000
+ADC_MAX_VALUE = 4096
+ADC_MAX_VOLTAGE = 3
+ADC_THRESHOLD_VALUE = ADC_MAX_VALUE * (float(c.get('ADC','THRESHOLD_VOLTAGE')) / ADC_MAX_VOLTAGE)
+ADC_POLLING_DELAY = float(c.get('ADC', 'POLLING_DELAY'))
 
-THRESHOLD_VALUE = (float(c.get('ADC','THRESHOLD_VOLTAGE')) / float(c.get('ADC','MAX_VOLTAGE'))) * float(c.get('ADC','MAX_VALUE'))
+BTN_MASK = 0b00001000
+BTN_POLLING_DELAY = float(c.get('BTN','POLLING_DELAY'))
 
 btn_state = False
 mic_state = False
-ldr_value = None
+ldr_value = 0
 
 def setLeds(yellow=False, green=False, red=False):
 	ledVals = 0x00
 	for idx, colour in enumerate((yellow, green, red)):
 		if(colour):
 			ledVals = ledVals | (0b1 << idx)
-	bus.write_byte(int(c.get('I2C','ADDR_B'), 16), ledVals)
+	bus.write_byte(I2C_ADDR_B, ledVals)
 
 
 # returns True if button was pressed since the last time getButton was called
@@ -46,37 +57,37 @@ def getButton():
 def checkButton():
 	global btn_state
 
-	byte = bus.read_byte(int(c.get('I2C', 'ADDR_B'), 16))
-	masked = int(c.get('BTN','MASK'), 2) & byte
+	byte = bus.read_byte(I2C_ADDR_B)
+	masked = BTN_MASK & byte
 
 	if masked == 0:
 		btn_state = True
 
-	threading.Timer(float(c.get('BTN','POLLING_DELAY')), checkButton).start()
+	threading.Timer(BTN_POLLING_DELAY, checkButton).start()
 checkButton()
 
 
 def applyADCMask(raw_result):
-	return (((raw_result & int(c.get('ADC','READ_MASK'), 2)) & int(c.get('ADC','UPPER_BYTE_MASK'), 2)) >> 8) | ((int(c.get('ADC','READ_MASK'), 2) & raw_result & ~int(c.get('ADC','UPPER_BYTE_MASK'), 2)) << 8)
+	return (((raw_result & ADC_READ_MASK) & ADC_UPPER_BYTE_MASK) >> 8) | ((ADC_READ_MASK & raw_result & ~ADC_UPPER_BYTE_MASK) << 8)
 
 
 def checkAdc():
 	global mic_state, ldr_value
 
-	bus.write_byte(0x21, 0b00010000)
-	mic = applyADCMask(bus.read_word_data(0x21, 0x00))
+	bus.write_byte(I2C_ADDR_A, ADC_COMMAND_CH1)
+	mic = applyADCMask(bus.read_word_data(I2C_ADDR_A, 0x00))
 
-	bus.write_byte(0x21, 0b00100000)
-	ldr = applyADCMask(bus.read_word_data(0x21, 0x00))
+	bus.write_byte(I2C_ADDR_A, ADC_COMMAND_CH2)
+	ldr = applyADCMask(bus.read_word_data(I2C_ADDR_A, 0x00))
 
 	print("MIC: " + str(mic))
 	print("LDR: " + str(ldr))
 
-	if mic > THRESHOLD_VALUE:
+	if mic > ADC_THRESHOLD_VALUE:
 		mic_state = True
 	ldr_value = ldr
 
-	threading.Timer(float(c.get('ADC', 'POLLING_DELAY')), checkAdc).start()
+	threading.Timer(ADC_POLLING_DELAY, checkAdc).start()
 checkAdc()
 
 # returns True if mic voltage exceeded threshold since last time getMic was called
@@ -88,9 +99,12 @@ def getMic():
 		return True
 	return mic_state
 
+def interpolateLdr(val):
+	return val / ADC_MAX_VALUE
+
 # unlike getMic and getButton, this just returns the latest value for the LDR
 def getLdr():
-	return ldr_value
+	return interpolateLdr(ldr_value)
 
 while True:
 	print(getMic())
--- Interfacing/pid.py
+++ Interfacing/pid.py
@@ -1,24 +1,32 @@
 import time
+import os
+import ConfigParser
+c = ConfigParser.ConfigParser()
+c.read('./constants.ini')
 
 class PID:
     #cb returns momentary sensor value
-    def __init__(self, cb, target, kp, ki, kd):
+    def __init__(self, cb, target):
         """
         Create pid instance. For coefficients see constants.ini
         :param cb: Function returning new values
         :param target: Value to try to maintain
-        :param kp: proportional coefficient
-        :param ki: integral coefficient
-        :param kd: derivative coefficient
         """
-        self.kp = kp
-        self.ki = ki
-        self.kd = kd
+        self.kp = float(c.get('PID', 'KP'))
+        self.kd = float(c.get('PID', 'KD'))
+        self.ki = float(c.get('PID', 'KI'))
         self.cb = cb
         self.target = target
         self.prev = None
 
-
+    def setTarget(self, target):
+        """
+        Sets new target value
+        :param target:
+        :return:
+        """
+        self.target = target
+        
     def start(self):
         """
         Initiates PID. Returns no value.
@@ -35,10 +43,12 @@ class PID:
         :return: Next value
         """
         t, val = time.time(), self.cb()
-        self.prevVal = \
+        result = \
             self.kp * (self.target - val) + \
             self.kd * ((val - self.prevVal) / (t - self.prevTime)) + \
             self.ki * ((val - self.prevVal) * (t - self.prevTime))
 
+        self.prevTime = t
+        self.prevVal = val
 
-pid = PID()
+        return result
