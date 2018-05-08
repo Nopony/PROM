101acc8934c315ea42455d6ed1fa46cbdf9895aa
perlinwarp <perlinwarp@gmail.com>
2018-05-08 16:57:34 +0100

    Made fast algorithm

--- Software/main.py
+++ Software/main.py
@@ -1,25 +1,28 @@
 from PIL import Image, ImageDraw
 import numpy as np
-import bugDetect
+import bugDetectFast
 import datetime
 import os
 
 while True:
 	#Opening an image without libs
 	name = "name"
-	os.system("fswebcam -r 640x480 --no-banner "+name+".jpg")
-	os.system("convert " + name + ".jpg -compress none " +name + ".ppm")
+	#os.system("fswebcam -r 640x480 --no-banner "+name+".jpg") #160x120
+	os.system("fswebcam -r 288x352 -S 4 --no-banner "+name+".jpg")
+	#fswebcam -d /dev/video0 -r 640x480 -S 4 --no-banner /home/pi/Animation/$FILENAME
 
 	image = Image.open(name+".jpg")
 	im2 = np.array(image)
 	blobs = []
-	bugs = bugDetect.detect(im2)
+	bugPos = bugDetectFast.detect(im2)
+	print(bugPos)
 
-	if not(len(bugs) == 0):
+	if not(bugPos == [100000000,100000000,0,0]):
 		print("Image: Cockroach Detected")
 		now = datetime.datetime.now()
 		print now.strftime("%Y-%m-%d %H:%M")
 		draw = ImageDraw.Draw(image)
-		for bug in bugs:
-			draw.rectangle([bug.minx,bug.miny,bug.maxx,bug.maxy])
+		draw.rectangle(bugPos)
 		image.save(now.strftime("%Y-%m-%d %H:%M")+".ppm")
+	else:
+		print("No bugs")
